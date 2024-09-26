9d5fb7a524d45054126f4326845a07ee6de8d074
psyKomicron <psykomicron@protonmail.com>
2024-09-26 12:22:51 +0200

    Version bump.

--- ClipboardManager/Resource.h
+++ ClipboardManager/Resource.h
@@ -14,7 +14,7 @@
 #define IDR_MAINFRAME                   128
 #define IDC_STATIC                      -1
 
-#define APP_VERSION                     L"0.4.2"
+#define APP_VERSION                     L"0.4.4"
 
 // Next default values for new objects
 // 
